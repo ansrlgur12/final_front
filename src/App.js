0addcb6e732c1bf1cf4715966a42980a859ee33c
10ESR10 <kyjin115@naver.com>
2023-06-15 19:40:34 +0900

    쇼핑페이지 수정

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ import MapMain from './page/Map/mapMain';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
 
-      
+     
       <Route path='/' element={<MainPage />} />
       <Route path='/shopMain' element={<ShopMain />} />
       <Route path='/mapMain' element={<MapMain />} />
