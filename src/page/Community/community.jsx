7e3f963f691f5627a159d31c12b05e1569535e81
jshblack <118979741+JSH1990@users.noreply.github.com>
2023-07-05 19:53:30 +0900

    네이버리뷰 수정

--- src/page/Community/community.jsx
+++ src/page/Community/community.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Header from "../../main/header";
 import ReviewCards from "./section/reviewCards";
+import SearchComponent from "../../util/SearchComponent";
+
 
 const Community = () => {
   return (
