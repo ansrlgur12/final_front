5e6b9519f949a9da356e6221c96e6474a363c391
ansrlgur12 <ansrlgur12>
2023-07-19 16:19:19 +0900

    0719

--- src/page/MyPage/section/userInfo.jsx
+++ src/page/MyPage/section/userInfo.jsx
@@ -22,12 +22,47 @@ const StyledContent = styled.div`
   background: #fff;
   border-radius: 5px;
   box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
   .img{
-    width: 4vw;
-    height: 4vw;
+    border: .5px solid #ccc;
+    border-radius: 50px;
+    padding: 1em;
+    width: 8vw;
+    height: 8vw;
+  }
+  .title{
+    color: rgb(74, 74, 74);
+    font-size: 2em;
+    margin-bottom: 2em;
   }
 `;
 
+const InformationBox = styled.div`
+  display: flex;
+  .left{
+    margin-right: 3em;
+  }
+  .right{
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+  }
+  .section{
+    display: flex;
+    flex-direction: row;
+  }
+  .desc{
+    margin-right: 2em;
+    font-weight: bold;
+    color:  #5e5e5e;;
+  }
+  .userInfo{
+    color: #2d6247;
+  }
+`;
 
 const UserInfo = () => {
   const token = Functions.getAccessToken();
@@ -65,7 +100,7 @@ const UserInfo = () => {
         <ImageFlexBox>
         <MyPageImageBar type = {"info"} />
         <StyledContent style={{ marginTop: '3vh' }}>
-          <Descriptions title="회원 정보">
+          {/* <Descriptions title="회원 정보">
           <Descriptions.Item label="프로필이미지">
             {userImg && <img className='img' src={userImg} alt="User" style={{backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: 'center'}} />}
           </Descriptions.Item>
@@ -75,7 +110,19 @@ const UserInfo = () => {
             <Descriptions.Item label="주소">
               {userAddr}
             </Descriptions.Item>
-          </Descriptions>
+          </Descriptions> */}
+          <div className='title'>회원정보</div>
+          <InformationBox>
+            <div className="left">
+              <img src={userImg} alt="" className="img" style={{backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: 'center'}}/>
+            </div>
+            <div className="right">
+              <div className="section"><div className='desc'>닉네임</div><div className='userInfo'>{nickName}</div></div>
+              <div className="section"><div className='desc'>전화번호</div><div className='userInfo'>{userPhoneNm}</div></div>
+              <div className="section"><div className='desc'>이메일</div><div className='userInfo'>{email}</div></div>
+              <div className="section"><div className='desc'>주소</div><div className='userInfo'>{userAddr}</div></div>
+            </div>
+          </InformationBox>
         </StyledContent>
         </ImageFlexBox>
       </StyledLayout>
